f54f60f241133de49fe24835f0375d1d748f5ce4
Precious <Preshomondi@gmail.com>
2022-06-26 06:48:50 +0300

    Create function getRandomMessage

--- script.js
+++ script.js
@@ -1,77 +1,10 @@
 
-// //Select elements
-// const API_URL = 'http://localhost:3000/books'
-// const form = document.getElementById('form')
-// const input = document.getElementById('input')
-// const main = document.getElementById('main') 
-
-// getBooks(API_URL)
-// //Create a function to get books
-// function getBooks(url) {
-//     fetch(url)
-//     .then(res => res.json)
-//     .then(books=> showBooks(books))
-// }
-
-// //Create a function to show the books and their properties
-// function showBooks(books) {
-//     main.innerHTML = ''
-//     books.forEach((book)=> {
-//         const {title, author, pages, description, image} = book
-//         const bookEl = document.createElement('div')
-//         bookEl.classList.add('book')
-
-
-//         bookEl.innerHTML = `
-//         <div class="book">
-//         <img src="${image}" alt="${title}">
-//         <div class="info">
-//            <h3>${title}</h3> 
-//            <h4>${author}</h4>
-//            <span class="rating">${pages}</span>
-//         </div>
-        
-//            <div class="synopsis">
-//             <div class="text">
-//                 <h3>synopsis</h3>
-//                 <p>${description}</p>
-//                </div>
-              
-//                <button class="add"><span>To read</span></button>
-//            </div>
-//     </div>
-
-//         `
-//         main.appendChild(bookEl)
-
-
-//     })
-// }
-
-
-
-
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
-
-//     const inputTerm = input.value
-
-//     if(inputTerm && inputTerm !== ' ') {
-//         getBooks(API_URL + inputTerm)
-
-//         input.value = " "
-//     }
-//     else {
-//         window.location.reload()
-//     }
-// })
-
 const API_URL = 'http://localhost:3000/books'
 
 const form = document.getElementById('form')
 const input = document.getElementById('input')
 const main = document.getElementById('main')
-//get initial movies
+//get the books
 
 getBooks(API_URL)
 
@@ -80,6 +13,8 @@ function getBooks(url) {
     .then(res => res.json())
     .then(books => showBooks(books))
 }
+
+//show the books
 
 function showBooks(books) {
     main.innerHTML = ""
@@ -98,8 +33,6 @@ function showBooks(books) {
             <div class="info">
                <h3>${title}</h3> 
                <h4>${author}</h4>
-               
-               
                <span class="rating">${pages}</span>
             </div>
             
@@ -119,17 +52,30 @@ function showBooks(books) {
 
 
 }
+//prevent form submission
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
 
-// form.addEventListener('submit', (e) => {
-//     e.preventDefault()
+    // const inputTerm = input.value
 
-//     const inputTerm = input.value
+    // if(inputTerm !== '') {
+    //     getBooks(API_URL + inputTerm)
 
-//     if(inputTerm && inputTerm !== '') {
-//         getBooks(API_URL + inputTerm)
+    //     input.value = ""
+    // }else {
+    //     window.location.reload()
+    // }
+})
 
-//         input.value = ""
-//     }else {
-//         window.location.reload()
-//     }
-// })
+//Mark the read books
+const noti = document.getElementsByClassName('fa-solid fa-book-open')
+const select = document.querySelector('.select')
+const button = document.querySelector('.add')
+
+for(but of button) {
+    but.addEventListener('click', (e) => {
+        const read = Number(noti.getAttribute('data-count') || 0)
+        noti.setAttribute('data-count', read + 1)
+        noti.classList.add('zero')
+    })
+} 
