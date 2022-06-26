19b3b2479fa38fbfd38cc2c4c4d1e5628f9ba152
Precious <Preshomondi@gmail.com>
2022-06-26 04:14:13 +0300

    Create function showBooks

--- script.js
+++ script.js
@@ -1,64 +1,55 @@
 
-//Select elements
-const API_URL = 'http://localhost:3000/books'
-const form = document.getElementById('form')
-const input = document.getElementById('input')
-const main = document.getElementById('main') 
-
-getBooks(API_URL)
-//Create a function to get books
-function getBooks() {
-    fetch(API_URL)
-    .then(res => res.json)
-    .then (books => showBooks(books))
-}
+// //Select elements
+// const API_URL = 'http://localhost:3000/books'
+// const form = document.getElementById('form')
+// const input = document.getElementById('input')
+// const main = document.getElementById('main') 
 
 // getBooks(API_URL)
-
+// //Create a function to get books
 // function getBooks(url) {
 //     fetch(url)
-//     .then(res => res.json())
-//     .then(books => showBooks(books))
+//     .then(res => res.json)
+//     .then(books=> showBooks(books))
 // }
 
+// //Create a function to show the books and their properties
 // function showBooks(books) {
-//     main.innerHTML = ""
-
-//    books.forEach((book) => {
+//     main.innerHTML = ''
+//     books.forEach((book)=> {
 //         const {title, author, pages, description, image} = book
-    
-      
-    
 //         const bookEl = document.createElement('div')
 //         bookEl.classList.add('book')
 
+
 //         bookEl.innerHTML = `
 //         <div class="book">
-//             <img src="${image}" alt="${title}">
-//             <div class="info">
-//                <h3>${title}</h3> 
-//                <h4>${author}</h4>
-               
-               
-//                <span class="rating">${pages}</span>
-//             </div>
-            
-//                <div class="synopsis">
-//                 <div class="text">
-//                     <h3>synopsis</h3>
-//                     <p>${description}</p>
-//                    </div>
-                  
-//                    <button class="add"><span>To read</span></button>
-//                </div>
+//         <img src="${image}" alt="${title}">
+//         <div class="info">
+//            <h3>${title}</h3> 
+//            <h4>${author}</h4>
+//            <span class="rating">${pages}</span>
 //         </div>
+        
+//            <div class="synopsis">
+//             <div class="text">
+//                 <h3>synopsis</h3>
+//                 <p>${description}</p>
+//                </div>
+              
+//                <button class="add"><span>To read</span></button>
+//            </div>
+//     </div>
+
 //         `
-
-//        main.appendChild(bookEl) 
-//     } )
+//         main.appendChild(bookEl)
 
 
+//     })
 // }
+
+
+
 
 // form.addEventListener('submit', (e) => {
 //     e.preventDefault()
@@ -71,6 +62,74 @@ function getBooks() {
 //         input.value = " "
 //     }
 //     else {
+//         window.location.reload()
+//     }
+// })
+
+const API_URL = 'http://localhost:3000/books'
+
+const form = document.getElementById('form')
+const input = document.getElementById('input')
+const main = document.getElementById('main')
+//get initial movies
+
+getBooks(API_URL)
+
+function getBooks(url) {
+    fetch(url)
+    .then(res => res.json())
+    .then(books => showBooks(books))
+}
+
+function showBooks(books) {
+    main.innerHTML = ""
+
+   books.forEach((book) => {
+        const {title, author, pages, description, image} = book
+    
+      
+    
+        const bookEl = document.createElement('div')
+        bookEl.classList.add('book')
+
+        bookEl.innerHTML = `
+        <div class="book">
+            <img src="${image}" alt="${title}">
+            <div class="info">
+               <h3>${title}</h3> 
+               <h4>${author}</h4>
+               
+               
+               <span class="rating">${pages}</span>
+            </div>
+            
+               <div class="synopsis">
+                <div class="text">
+                    <h3>synopsis</h3>
+                    <p>${description}</p>
+                   </div>
+                  
+                   <button class="add"><span>To read</span></button>
+               </div>
+        </div>
+        `
+
+       main.appendChild(bookEl) 
+    } )
+
+
+}
+
+// form.addEventListener('submit', (e) => {
+//     e.preventDefault()
+
+//     const inputTerm = input.value
+
+//     if(inputTerm && inputTerm !== '') {
+//         getBooks(API_URL + inputTerm)
+
+//         input.value = ""
+//     }else {
 //         window.location.reload()
 //     }
 // })
